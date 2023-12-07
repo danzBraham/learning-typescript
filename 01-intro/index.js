6722b44de1abe88976dbb4510b5369b3ad8977bb
Zidan Abraham <zidangit07@gmail.com>
2023-12-07 19:59:13 +0800

    feat(folder): add 01-intro folder

---
+++ 01-intro/index.js
@@ -0,0 +1,6 @@
+var user = {
+    name: 'Zidan',
+    age: 19,
+};
+console.log(user.name);
+console.log(user.age);
---
+++ 01-intro/index.ts
@@ -0,0 +1,7 @@
+const user = {
+  name: 'Zidan',
+  age: 19,
+};
+
+console.log(user.name);
+console.log(user.age);
