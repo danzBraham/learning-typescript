61d6c52a956e0dd4a87a652452badc39f5a21e23
Zidan Abraham <zidangit07@gmail.com>
2023-12-09 09:04:28 +0800

    refactor(folder): rename 01-intro to 01-basics and remove redundant index files

--- 01-intro/00-index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var user = {
-    name: 'Zidan',
-    age: 19,
-};
-console.log(user.name);
-console.log(user.age);
--- 01-intro/00-index.ts
+++
@@ -1,9 +0,0 @@
-const user = {
-  name: 'Zidan',
-  age: 19,
-};
-
-console.log(user.name);
-console.log(user.age);
-
-export {};
